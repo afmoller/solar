64081e442f8f0e196c7e4dac1d9faadac321af8c
Fredrik Möller <a.fredrik.moller@gmail.com>
2025-12-21 20:08:42 +0100

    Optimizations on the Weather dashboard.

--- solar-frontend/src/app/features/dashboards/weather/weather.component.ts
+++ solar-frontend/src/app/features/dashboards/weather/weather.component.ts
@@ -70,7 +70,7 @@ export class WeatherComponent implements OnInit {
     
     entries.push({key: 'Temperture', value: data.temperatureOutdoorInCelsius.toFixed(1) + '°C'});
     entries.push({key: 'Humidity', value: data.humidityOutdoor.toFixed(0) + '%'});
-    entries.push({key: 'Solarradiation', value: data.solarRadiation.toFixed(0) + ' w/m2'});
+    entries.push({key: 'Solarradiation', value: data.solarRadiation.toFixed(1) + ' w/m2'});
     entries.push({key: 'UV Index', value: data.uv.toFixed(0)});
     
     return entries;
@@ -79,12 +79,12 @@ export class WeatherComponent implements OnInit {
    getRainEntries(data: Weatherentry): WeatherTableentry[] {
     let entries = new Array<WeatherTableentry>();
     
-    entries.push({key: 'Rate', value: data.rainRateInMillimetersPerHour.toFixed(0) + ' mm/h'});
-    entries.push({key: 'Event', value: data.eventRainInMillimeters.toFixed(0) + ' mm'});
-    entries.push({key: 'Hourly', value: data.hourlyRainInMillimeters.toFixed(0) + ' mm'});
+    entries.push({key: 'Rate', value: data.rainRateInMillimetersPerHour.toFixed(1) + ' mm/h'});
+    entries.push({key: 'Event', value: data.eventRainInMillimeters.toFixed(1) + ' mm'});
+    entries.push({key: 'Hourly', value: data.hourlyRainInMillimeters.toFixed(1) + ' mm'});
     entries.push({key: 'Daily', value: data.dailyRainInMillimeters.toFixed(1) + ' mm'});
-    entries.push({key: 'Weekly', value: data.weeklyRainInMillimeters.toFixed(0) + ' mm'});
-    entries.push({key: 'Monthly', value: data.monthlyRainInMillimeters.toFixed(0) + ' mm'});
+    entries.push({key: 'Weekly', value: data.weeklyRainInMillimeters.toFixed(1) + ' mm'});
+    entries.push({key: 'Monthly', value: data.monthlyRainInMillimeters.toFixed(1) + ' mm'});
     entries.push({key: 'Yearly', value: data.yearlyRainInMillimeters.toFixed(0) + ' mm'});
     
     return entries;
@@ -95,8 +95,8 @@ export class WeatherComponent implements OnInit {
     
     entries.push({key: 'Wind speed', value: data.windSpeedInMeterPerSecond.toFixed(1) + ' m/s'});
     entries.push({key: 'Wind Direction', value: data.windDirection.toFixed(0) + '°'});
-    entries.push({key: 'Gust', value: data.windGustInMeterPerSecond.toFixed(0) + ' m/s'});
-    entries.push({key: 'Max Daily Gust', value: data.maxDailyGustInMeterPerSecond.toFixed(0) + ' m/s'});
+    entries.push({key: 'Gust', value: data.windGustInMeterPerSecond.toFixed(1) + ' m/s'});
+    entries.push({key: 'Max Daily Gust', value: data.maxDailyGustInMeterPerSecond.toFixed(1) + ' m/s'});
     
     return entries;
   }
@@ -108,12 +108,42 @@ export class WeatherComponent implements OnInit {
     entries.push({key: 'Heap', value: data.heap.toFixed(0) + ' B'});
     entries.push({key: 'Interval', value: data.interval.toFixed(0) + 's'});
     entries.push({key: 'Model', value: data.model});
-    entries.push({key: 'Runtime', value: data.runtime.toFixed(0) + ' s'});
+    entries.push({key: 'Runtime', value: data.runtime.toFixed(0) + ' s (' + this.toHumanReadable(data.runtime) + ')'});
     entries.push({key: 'Station type', value: data.stationType});
     entries.push({key: 'wh25 Batt', value: data.wh25Batt.toFixed(0)});
     entries.push({key: 'wh65 Batt', value: data.wh65Batt.toFixed(0)});
     
     return entries;
+  }
+
+  toHumanReadable(seconds: number): string {
+    seconds = seconds || 0;
+    seconds = Number(seconds);
+    seconds = Math.abs(seconds);
+
+    const d = Math.floor(seconds / (3600 * 24));
+    const h = Math.floor(seconds % (3600 * 24) / 3600);
+    const m = Math.floor(seconds % 3600 / 60);
+    const s = Math.floor(seconds % 60);
+    const parts = [];
+
+    if (d > 0) {
+      parts.push(d + ' d');
+    }
+
+    if (h > 0) {
+      parts.push(h + ' h');
+    }
+
+    if (m > 0) {
+      parts.push(m + ' m');
+    }
+
+    if (s > 0) {
+      parts.push(s + ' s');
+    }
+
+    return parts.join(', ');
   }
 }
 
